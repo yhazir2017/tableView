e7ba32f454e86104e3eaeff6e5d02bb38926b05f
yhazir2017 <yildiray.hazir@gmail.com>
2017-08-17 20:40:16 -0700

    final commit

--- tableView/ViewController.swift
+++ tableView/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    let list = ["Milk","Honey", "Bread", "Cofffe", "Tomatoes"]
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return list.count
+        
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        cell.textLabel?.highlightedTextColor = UIColor.blue
+        cell.textLabel?.textColor = UIColor.red
+        return(cell)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
